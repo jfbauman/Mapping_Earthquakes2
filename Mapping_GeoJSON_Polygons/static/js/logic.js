a3aed1153f829d33b7a8b61be95a2bddef377ed1
Jessica Bauman <jbauman815@gmail.com>
2023-01-31 14:50:39 -0500

    adding torontohoods

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -39,7 +39,8 @@ L.control.layers(baseMaps).addTo(map);
 
 // Accessing the Toronto airline routes GeoJSON URL.
 let torontoData = "https://raw.githubusercontent.com/jfbauman/Mapping_Earthquakes2/Mapping_GeoJSON_Polygons/torontoRoutes.json";
-
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/jfbauman/Mapping_Earthquakes2/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 // Create a style for the lines.
 let myStyle = {
     color: "#ffffa1",
@@ -47,16 +48,17 @@ let myStyle = {
 }
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
     console.log(data);
     // Creating a GeoJSON layer with the retrieved data. 
-L.geoJSON(data, {
-    style: myStyle, 
-    color: "#ffffa1", 
-    weight: 2, 
-    onEachFeature: function(feature, layer) {
-        layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
-    }
-})
-.addTo(map);
+L.geoJSON(data).addTo(map);
 });
+//     style: myStyle, 
+//     color: "#ffffa1", 
+//     weight: 2, 
+//     onEachFeature: function(feature, layer) {
+//         layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
+//     }
+// })
+// .addTo(map);
+// });
